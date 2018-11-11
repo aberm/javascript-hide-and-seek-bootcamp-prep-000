6b4851f6b1dd09c5bc2f19ed1a719c09f0516f72
Menachem Aber <abermendel@gmail.com>
2018-11-11 03:11:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  var boo = document.querySelector('div#grand-node');
+  
+}
