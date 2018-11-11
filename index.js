271f041c6d768a6586ea26e1ebb5d1b089875143
Menachem Aber <abermendel@gmail.com>
2018-11-11 03:47:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  var y = document.querySelector('div#grand-node');
+  function retrieve(x){
+    while (x.children[0]) {
+    var a = x.children[0];
+    x = a; }
+    return x}
+return retrieve(y);
+}
